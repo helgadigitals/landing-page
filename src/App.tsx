32f86c2734aaed8fec92e52143ad5b7ad9762ddb
lemasani <brianlemasan@gmail.com>
2024-12-26 12:27:01 +0300

    routing, and dummy pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import Hero from './pages/Hero.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout.tsx';
+import { routes } from './lib/routes.tsx'; // Import the route configuration
 
+// Create the router dynamically
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: routes.map(({ path, element }) => ({ path: path === '/' ? '' : path.slice(1), element })),
+    },
+]);
 
 function App() {
-
-  return (
-    <>
-      <Hero />
-    </>
-  )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home (){
+	return (
+	  <>
+		<section className='hero bg-yellow-400 min-h-screen'>
+			<div className='container mx-auto px-6 flex justify-center items-center'>
+				<h1 className='text-2xl text-white-500'>Landing company</h1>
+			</div>
+			
+			
+		</section>
+	  </>
+	)
+}
